5fc6de6bfe21a326a125aab27fdd74f4df825893
KennTorg <91532197+KennTorg@users.noreply.github.com>
2022-01-07 15:40:42 +0100

    123

---
+++ js/script.js
@@ -0,0 +1,47 @@
+/* ------ Javascript ------ */
+
+/* Task 1 */
+// string
+var cryptocoin = "Bitcoin";
+console.log(cryptocoin);
+// number
+var age = 30;
+console.log(age);
+// boolean
+var iAmRich = false;
+console.log(iAmRich);
+
+/* Task 2 */
+// FirstName
+var firstName = "Kennet";
+// LastName
+var lastName = "Torgersen";
+// Names combined:
+var fullName = firstName + " " + lastName;
+console.log(fullName);
+
+/* Task 3 */
+//Var
+var animal = "frog";
+console.log(animal);
+
+typeof animal;
+/* I DONT GET IT .......... so annoying */
+
+/* Task 4 */
+// Var
+var orderHasShipped = true;
+// If the order was shipped
+if ((orderHasShipped = true)) {
+  console.log("The order shipped");
+}
+// If the order was not shipped
+else {
+  console.log("The order did not ship");
+}
+
+/* Task 5 */
+// Counting  + 1 from 7 to 13
+for (var i = 7; i <= 13; i++) {
+  console.log(i);
+}
